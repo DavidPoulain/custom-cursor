c1d38b2a5a8d582d44cd6da90e5ccd5c4313ee37
DavidPoulain <david.poulain60@gmail.com>
2022-02-22 15:46:58 +0100

    tentative de display non sur body

---
+++ js/cursor.js
@@ -0,0 +1,11 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top: '+(e.pageY -10)+'px; left: '+(e.pageX -10)+'px;')
+})
+
+// document.body.addEventListener('mouseout', removeMouse);
+
+// function removeMouse() {
+//     console.log('ok');
+// }
