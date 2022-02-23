c3381b82c49b0a107ec67700ed4d40d8e8723e61
DavidPoulain <david.poulain60@gmail.com>
2022-02-23 11:33:11 +0100

    script avant fermeture body

--- V1/js/cursor.js
+++ V1/js/cursor.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top: '+(e.pageY -10)+'px; left: '+(e.pageX -10)+'px;')
+    cursor.setAttribute('style', 'top: '+(e.pageY -15)+'px; left: '+(e.pageX -15)+'px;')
 })
 
 document.body.addEventListener('mouseleave', removeMouse);
