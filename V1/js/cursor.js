8eab4ad86d808f08ce10f9feae99b7f7f3a36cdf
DavidPoulain <david.poulain60@gmail.com>
2022-03-13 14:07:50 +0100

    cursor display none when over video

--- V1/js/cursor.js
+++ V1/js/cursor.js
@@ -1,13 +1,15 @@
 const cursor = document.querySelector('.cursor');
+const iframe = document.querySelector("iframe");
 
 document.addEventListener('mousemove', e => {
     cursor.setAttribute('style', 'top: '+(e.pageY -15)+'px; left: '+(e.pageX -15)+'px;')
 })
 
 document.body.addEventListener('mouseleave', removeMouse);
+iframe.addEventListener("mouseover", removeMouse);
+iframe.addEventListener("mouseleave", addMouse);
 
 function removeMouse(event) {
-    console.log('ok');
     const cursorElement = document.querySelector('.cursor');
     cursorElement.classList.add('cursor--no');
 }
@@ -18,3 +20,5 @@ function addMouse(event) {
     const cursorElement = document.querySelector('.cursor');
     cursorElement.classList.remove('cursor--no');
 }
+
+
